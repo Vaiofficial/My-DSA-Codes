abe0fa49d8b0a40b5f8603b0f14347409d6d38b9
Vaiofficial <pvaibhavofficial@gmail.com>
2023-06-08 00:32:55 +0530

    left first rotate

--- Arrays/leftrotate.cpp
+++ Arrays/leftrotate.cpp
@@ -1,31 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rotate(vector<int> &arr, int k,int n)
+int rotate(int arr[], int k, int n) // 1 2 3 4 5 6 7
 {
-    k=k%n;
-	   if(k==0) {
-	       return;
-	   }
-	   int arr1[k];
-	   for(int i=0;i<k;i++) {
-	       arr1[i]=arr[i];
-	   }
-	   for(int i=k;i<n;i++) {
-	       arr[i-k]=arr[i];
-	   }
-	   for(int i=n-k;i<n;i++) {
-	       arr[i]=arr1[i-n+k];
-	   }
+    // pushing the first element of the array into variable first
+    int first = arr[0];
+
+    // loop to push element one by one to its one position before .
+    for (int i = 0; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+
+    // pushing the first element of the array into the last position.
+    arr[n - 1] = first;
 }
 
+int printarray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 
 int main()
 {
-    vector<int>arr= {1 ,2 ,3 ,4 ,5 ,6 ,7};
-    int n=7,k=2;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = 7, k = 2;
 
-    rotate(arr,k);
+    // calling rotate function
+    rotate(arr, k, n);
+
+    // printing the final array
+    printarray(arr,n);
 
     return 0;
 }
