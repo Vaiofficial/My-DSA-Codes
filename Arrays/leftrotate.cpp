85d812f53958209d596c2c3c2b0e2b9a5ef79181
Vaiofficial <pvaibhavofficial@gmail.com>
2023-06-08 00:44:18 +0530

    rotate changes

--- Arrays/leftrotate.cpp
+++ Arrays/leftrotate.cpp
@@ -7,7 +7,7 @@ int rotate(int arr[], int k, int n) // 1 2 3 4 5 6 7
     int first = arr[0];
 
     // loop to push element one by one to its one position before .
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
         arr[i - 1] = arr[i];
     }
