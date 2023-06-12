0a9b115f20de04f8aa007bb73ffe3fb41db58905
Vaiofficial <pvaibhavofficial@gmail.com>
2023-06-12 12:21:31 +0530

    missing number using sum method

---
+++ Arrays/missing_sum_optmize.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int> &nums)
+{
+
+    int sum = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += nums[i];
+    }
+
+    int total = (n * (n + 1)) / 2;
+    int result = total - sum;
+
+    return result;
+}
+
+int main()
+{
+
+    vector<int> arr = {0 ,1, 2, 3, 5};
+    int missing = missingNumber(arr);
+    std::cout << "the missing number is " << missing << std::endl;
+
+    return 0;
+}
