a27626c8997341b482076dca22db3d6ba1221fa8
Vaiofficial <pvaibhavofficial@gmail.com>
2023-06-14 13:08:03 +0530

    long subarray better

--- Arrays/long_subarray_better.cpp
+++ Arrays/long_subarray_better.cpp
@@ -1,37 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longsubarray(int a[], int  n, long long k)
+int longsubarray(int a[], int n, long long k)
 {
     map<long long, int> presum;
     long long sum = 0;
     int maxlen = 0;
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        sum+=a[i];
-        if(sum == k)
+        sum += a[i];
+        if (sum == k)
         {
-            maxlen = max(maxlen , i+1);
+            maxlen = max(maxlen, i + 1);
         }
 
-        int rem = sum-k;
-        if(presum.find(rem)!= presum.end())
+        int rem = sum - k;
+        if (presum.find(rem) != presum.end())
         {
             int len = i - presum[rem];
-            maxlen = max(maxlen , len);
+            maxlen = max(maxlen, len);
         }
-        presum[sum] = i;
+        if (presum.find(sum) == presum.end())
+        {
+            presum[sum] = i;
+        }
     }
     return maxlen;
 }
 
 int main()
 {
-    int arr[] = {10, 5, 2, 7, 1, 9},n=6,k=15;
+    int arr[] = {2,0,0,3}, n = 4, k = 3;
 
-    int ans = longsubarray(arr , n , k);
-    cout<<ans;
+    int ans = longsubarray(arr, n, k);
+    cout << ans;
 
     return 0;
 }
