a2b441a212e6b1845b15c41d76ac4477024bcc4d
Vaiofficial <pvaibhavofficial@gmail.com>
2023-06-14 12:22:28 +0530

    better long subarray

--- Arrays/longest_subarrayBF.cpp
+++ Arrays/longest_subarrayBF.cpp
@@ -6,11 +6,11 @@ int longsubarray(int arr[], int k, int n)
 
     int i, sum, maxlen = 0;
 
-    //starting with the first element 
+    // starting with the first element
     for (int i = 0; i < n; i++)
     {
         sum = 0;
-        //adding next element starting from i=0 , i.e first element and checking if sum == k if yes then finding the lenght of the subarray starting from i=0 to the point where we got sum ==15.
+        // adding next element starting from i=0 , i.e first element and checking if sum == k if yes then finding the lenght of the subarray starting from i=0 to the point where we got sum ==15.
         for (int j = i; j < n; j++)
         {
             sum = sum + arr[j];
@@ -30,7 +30,6 @@ int main()
     int arr[] = {10, 5, 2, 7, 1, 9};
     int k = 15;
     int n = 6;
-
     int len = longsubarray(arr, k, n);
     cout << len;
 
